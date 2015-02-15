db1208afc71a36e3f3577e7e0bab5eea817eb302
philopon <philopon.dependence@gmail.com>
2015-02-16 03:09:50 +0900

    purescript-0.6.7.1

--- purescript.rb
+++ purescript.rb
@@ -1,8 +1,8 @@
 class Purescript < Formula
   homepage "http://www.purescript.org"
-  url "https://github.com/purescript/purescript/releases/download/v0.6.7/macos.tar.gz"
-  sha1 "f4734f0fde11b9572f8d7c0036d519bb8cd1860f"
-  version "0.6.7"
+  url "https://github.com/purescript/purescript/releases/download/v0.6.7.1/macos.tar.gz"
+  sha1 "5ae15ae84d8fc8c4f2c2d8afddbc63fb523cc833"
+  version "0.6.7.1"
 
 
   def install
