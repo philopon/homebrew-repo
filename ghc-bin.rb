cf4175ccf0b15d26b05ea37875e6c1f52120fce3
philopon <philopon.dependence@gmail.com>
2015-03-02 22:37:26 +0900

    add ghc-bin

---
+++ ghc-bin.rb
@@ -0,0 +1,10 @@
+class GhcBin < Formula
+  homepage "https://www.haskell.org/ghc"
+  url "https://www.haskell.org/ghc/dist/7.8.3/ghc-7.8.3-x86_64-apple-darwin.tar.xz"
+  sha1 "d55a671482614cbd3c12e4594437aedfd78f47de"
+
+  def install
+    system "./configure", "--prefix=#{prefix}"
+    system "make", "install"
+  end
+end
