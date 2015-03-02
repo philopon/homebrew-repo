e02f884032b5b3c36ecb75eb3207727cd0c12502
philopon <philopon.dependence@gmail.com>
2015-03-03 02:08:40 +0900

    fix ghc

--- ghc-bin.rb
+++ ghc-bin.rb
@@ -3,6 +3,8 @@ class GhcBin < Formula
   url "https://www.haskell.org/ghc/dist/7.8.3/ghc-7.8.3-x86_64-apple-darwin.tar.xz"
   sha1 "d55a671482614cbd3c12e4594437aedfd78f47de"
 
+  env :std
+
   def install
     system "./configure", "--prefix=#{prefix}"
     system "make", "install"
