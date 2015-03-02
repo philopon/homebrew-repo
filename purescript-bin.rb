30aa20ab709163f016c5c89392456cb044b9e230
philopon <philopon.dependence@gmail.com>
2015-03-02 22:29:00 +0900

    fix purescript-bin

--- purescript-bin.rb
+++ purescript-bin.rb
@@ -1,9 +1,8 @@
-class Purescript < Formula
+class PurescriptBin < Formula
   homepage "http://www.purescript.org"
   url "https://github.com/purescript/purescript/releases/download/v0.6.8/macos.tar.gz"
   sha1 "4c5e4d08bd76983a4bfd7fa8340f30e685193e49"
   version "0.6.8"
-
 
   def install
     bin.install "psc", "psc-docs", "psc-make", "psci"
