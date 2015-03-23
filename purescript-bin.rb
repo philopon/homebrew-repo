4bbed2a5e6177e8939d1b6e78ac48979d822c26e
philopon <philopon.dependence@gmail.com>
2015-03-23 20:02:26 +0900

    purescript-0.6.9.3

--- purescript-bin.rb
+++ purescript-bin.rb
@@ -1,8 +1,8 @@
 class PurescriptBin < Formula
   homepage "http://www.purescript.org"
-  url "https://github.com/purescript/purescript/releases/download/v0.6.8/macos.tar.gz"
-  sha1 "4c5e4d08bd76983a4bfd7fa8340f30e685193e49"
-  version "0.6.8"
+  url "https://github.com/purescript/purescript/releases/download/v0.6.9.3/macos.tar.gz"
+  sha1 "b1e8f4d5b33398941deeb9867cc08776d3b18671"
+  version "0.6.9.3"
 
   conflicts_with "purescript"
 
