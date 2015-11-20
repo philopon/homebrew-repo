57f6683c29300a03d4db5e8032b7005e233e3210
Hirotomo Moriwaki <philopon.dependence@gmail.com>
2015-11-20 20:57:49 +0900

    cleanup

--- cabal-install-bin.rb
+++
@@ -1,9 +0,0 @@
-class CabalInstallBin < Formula
-  homepage "https://www.haskell.org/cabal/"
-  url "https://www.haskell.org/cabal/release/cabal-install-1.22.0.0/cabal-1.22.0.0-x86_64-apple-darwin-mavericks.tar.gz"
-  sha1 "f290db731fec9679f60228bc77cb1a9693d0719a"
-
-  def install
-    bin.install "cabal"
-  end
-end
