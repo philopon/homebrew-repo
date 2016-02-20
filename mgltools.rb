32eb80a9339213c00f478a0751115bbddfb9b32d
Hirotomo Moriwaki <philopon.dependence@gmail.com>
2016-02-20 15:23:45 +0900

    mgltools: modify shebang

--- mgltools.rb
+++ mgltools.rb
@@ -1,15 +1,33 @@
 class Mgltools < Formula
-  desc "software for visualization and analysis of molecular structures."
+  desc "visualization and analysis of molecular structures."
   homepage "http://mgltools.scripps.edu/"
   url "http://mgltools.scripps.edu/downloads/downloads/tars/releases/REL1.5.6/mgltools_i86Darwin9_1.5.6.tar.gz"
   sha256 "15f9f64015270bfa6fa3091c7278e442a77e73d5db6d20ffe45ae019294a3d17"
 
   keg_only "require in package python"
 
+  depends_on "openssl"
   depends_on :x11
 
   def install
     system "./install.sh", "-d", prefix
+  end
+
+  def post_install
+    Dir["#{prefix}/MGLToolsPckgs/AutoDockTools/Utilities24/*.py"].each do |script|
+
+      cnt = "#!/usr/local/opt/mgltools/bin/python\n"
+      File.foreach(script) do |line|
+        if /^#!/ !~ line then
+          cnt += line
+        end
+      end
+
+      File.open(script, "w") do |file|
+        file.puts(cnt)
+      end
+
+    end
   end
 
   test do
