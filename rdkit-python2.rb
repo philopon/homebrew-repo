ef0e2f23254375aff4efe778b4433a94092a7a09
Hirotomo Moriwaki <philopon.dependence@gmail.com>
2016-07-09 23:54:10 +0900

    split rdkit

--- rdkit-python2.rb
+++ rdkit-python2.rb
@@ -1,7 +1,7 @@
 class RdkitPython2 < Formula
   homepage "http://rdkit.org/"
-  url "https://github.com/rdkit/rdkit/archive/Release_2015_09_2.tar.gz"
-  sha256 "2ad97ec2de97729c483b89cd65470d828a4be84c88305043660cc524a86fd053"
+  url "https://github.com/rdkit/rdkit/archive/Release_2016_03_2.tar.gz"
+  sha256 "f9531c9a03da3c446ad53cc56cf2d4762bf81088dd69fe07bdd41bbd87221ca3"
 
   keg_only "multiple version"
 
@@ -13,14 +13,9 @@ class RdkitPython2 < Formula
   depends_on "homebrew/python/numpy"
 
   def install
-    args = std_cmake_parameters.split
-    args << "-DRDK_INSTALL_INTREE=OFF"
-
     cd "External/INCHI-API" do
       system "bash", "download-inchi.sh"
     end
-
-    args << "-DRDK_BUILD_INCHI_SUPPORT=ON"
 
     python_executable = "#{HOMEBREW_PREFIX}/bin/python2"
     python_prefix = `#{python_executable} -c "import sys;print(sys.prefix)"`.chomp
@@ -28,23 +23,21 @@ class RdkitPython2 < Formula
 
     python_version = `#{python_executable} -c "import sys; print('.'.join(map(str, sys.version_info[:2])))"`.chomp
     site_packages = "lib/python#{python_version}/site-packages"
+    numpy_include = `#{python_executable} -c "import numpy;print(numpy.get_include())"`.chomp
+
+    args = std_cmake_parameters.split
+    args << "-DRDK_INSTALL_INTREE=OFF"
+    args << "-DRDK_BUILD_INCHI_SUPPORT=ON"
 
     args << "-DPYTHON_EXECUTABLE='#{python_executable}'"
     args << "-DPYTHON_INCLUDE_DIR='#{python_include}'"
     args << "-DPYTHON_LIBRARY='#{python_prefix}/Python'"
-
-    numpy_include = `#{python_executable} -c "import numpy;print(numpy.get_include())"`.chomp
     args << "-DPYTHON_NUMPY_INCLUDE_PATH='#{numpy_include}'"
-
     args << "."
 
     system "cmake", *args
     system "make"
     system "make", "install"
-
-    File.open(HOMEBREW_PREFIX/site_packages/"rdkit-python2.pth", "w") do |file|
-      file.puts(opt_prefix/site_packages)
-    end
   end
 
   test do
