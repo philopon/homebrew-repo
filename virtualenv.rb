38224568a0d42a7c94ebfa778cce28103a11edb1
Hirotomo Moriwaki <philopon.dependence@gmail.com>
2017-01-14 00:35:25 +0900

    virtualenv-15.1.0

--- virtualenv.rb
+++ virtualenv.rb
@@ -1,8 +1,8 @@
 class Virtualenv < Formula
   desc "tool to create isolated Python environments"
   homepage "https://virtualenv.pypa.io"
-  url "https://pypi.python.org/packages/source/v/virtualenv/virtualenv-14.0.6.tar.gz"
-  sha256 "1ffb6a02d8999e9c97ad8f04b1d2ba44421dfb8f8a98b54aea5c6fdfb53bc526"
+  url "https://pypi.python.org/packages/d4/0c/9840c08189e030873387a73b90ada981885010dd9aea134d6de30cd24cb8/virtualenv-15.1.0.tar.gz"
+  sha256 "02f8102c2436bb03b3ee6dede1919d1dac8a427541652e5ec95171ec8adbc93a"
 
   depends_on :python
 
